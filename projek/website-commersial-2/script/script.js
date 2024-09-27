96d9850d83a45ffec228c9b327bb1bb7bf5f5983
Fajar Saputra <fajarsaputra9513@gmail.com>
2024-09-27 22:41:24 +0700

    making a website coffee shop

---
+++ projek/website-commersial-2/script/script.js
@@ -0,0 +1,25 @@
+// search
+const searchBox = document.querySelector(".search-box");
+
+document.querySelector("#search-icon").onclick = () => {
+    navbarNav.classList.toggle("active");
+};
+
+console.log(searchBox);
+
+// toggle
+// const navbarNav = document.querySelector(".navbar-nav");
+
+// // ketika diklik
+// document.querySelector("#hamburger-menu").onclick = () => {
+//     navbarNav.classList.toggle("active");
+// };
+
+// // ketika klik diluar sidebar
+// const hamburger = document.querySelector("#hamburger-menu");
+
+// document.addEventListener("click", function (e) {
+//     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//         navbarNav.classList.remove("active");
+//     }
+// });
