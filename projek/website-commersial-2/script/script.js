326077ae0bba98156b376b868fdde00066dbee11
Fajar Saputra <fajarsaputra9513@gmail.com>
2024-09-30 22:01:19 +0700

    modif breakpoint website-commersial-2

--- projek/website-commersial-2/script/script.js
+++ projek/website-commersial-2/script/script.js
@@ -14,6 +14,21 @@ document.addEventListener("click", function (e) {
     }
 });
 
+// Toggle
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+    if (!navbarNav.contains(e.target) && !hamburger.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
+
 // toggle
 // const navbarNav = document.querySelector(".navbar-nav");
 
