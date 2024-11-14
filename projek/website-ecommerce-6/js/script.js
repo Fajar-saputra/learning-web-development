4cd1fe59be9c2780b4ba0605595993f8e8b9749c
Fajar Saputra <fajarsaputra2305@gmail.com>
2024-11-14 17:17:43 +0700

    add fitur responsive mobile nav for website-ecommerce-6

---
+++ projek/website-ecommerce-6/js/script.js
@@ -0,0 +1,40 @@
+// copy menu for mobile 
+function copyMenu(){
+    const dptCategory = document.querySelector('.dpt-cat');
+    const dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+// copy inside nav to nav
+    const mainNav = document.querySelector('.header-nav nav');
+    const navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+// copy .header-top .wrapper to thetop-nav
+    const topNav = document.querySelector('.header-top .wrapper');
+    const topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+// show mobile menu
+const menuButton = document.querySelector(".trigger"),
+    closeButton = document.querySelector(".t-close"),
+    addClass = document.querySelector(".site");
+
+menuButton.addEventListener("click", function() {
+    addClass.classList.toggle("showmenu");
+});
+closeButton.addEventListener("click", function() {
+    addClass.classList.remove("showmenu");
+});
+
+// show sub menu on mobile
+const subMenu = document.querySelectorAll('.has-child .icon-small');
+subMenu.forEach(menu => menu.addEventListener('click', toggle));
+
+function toggle(e){
+    e.preventDefault();
+    subMenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if(this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+}
