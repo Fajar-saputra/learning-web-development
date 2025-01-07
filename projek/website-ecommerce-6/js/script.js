aae0d1808fe209ef4c28090a396ad0b0cfa5c8ce
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-01-07 22:58:10 +0700

    tidying up the source code

--- projek/website-ecommerce-6/js/script.js
+++ projek/website-ecommerce-6/js/script.js
@@ -1,17 +1,17 @@
-// copy menu for mobile 
-function copyMenu(){
-    const dptCategory = document.querySelector('.dpt-cat');
-    const dptPlace = document.querySelector('.departments');
+// copy menu for mobile
+function copyMenu() {
+    const dptCategory = document.querySelector(".dpt-cat");
+    const dptPlace = document.querySelector(".departments");
     dptPlace.innerHTML = dptCategory.innerHTML;
 
-// copy inside nav to nav
-    const mainNav = document.querySelector('.header-nav nav');
-    const navPlace = document.querySelector('.off-canvas nav');
+    // copy inside nav to nav
+    const mainNav = document.querySelector(".header-nav nav");
+    const navPlace = document.querySelector(".off-canvas nav");
     navPlace.innerHTML = mainNav.innerHTML;
 
-// copy .header-top .wrapper to thetop-nav
-    const topNav = document.querySelector('.header-top .wrapper');
-    const topPlace = document.querySelector('.off-canvas .thetop-nav');
+    // copy .header-top .wrapper to thetop-nav
+    const topNav = document.querySelector(".header-top .wrapper");
+    const topPlace = document.querySelector(".off-canvas .thetop-nav");
     topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
@@ -21,22 +21,22 @@ const menuButton = document.querySelector(".trigger"),
     closeButton = document.querySelector(".t-close"),
     addClass = document.querySelector(".site");
 
-menuButton.addEventListener("click", function() {
+menuButton.addEventListener("click", function () {
     addClass.classList.toggle("showmenu");
 });
-closeButton.addEventListener("click", function() {
+closeButton.addEventListener("click", function () {
     addClass.classList.remove("showmenu");
 });
 
 // show sub menu on mobile
-const subMenu = document.querySelectorAll('.has-child .icon-small');
-subMenu.forEach(menu => menu.addEventListener('click', toggle));
+const subMenu = document.querySelectorAll(".has-child .icon-small");
+subMenu.forEach((menu) => menu.addEventListener("click", toggle));
 
-function toggle(e){
+function toggle(e) {
     e.preventDefault();
-    subMenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
-    if(this.closest('.has-child').classList != 'expand');
-    this.closest('.has-child').classList.toggle('expand');
+    subMenu.forEach((item) => (item != this ? item.closest(".has-child").classList.remove("expand") : null));
+    if (this.closest(".has-child").classList != "expand");
+    this.closest(".has-child").classList.toggle("expand");
 }
 
 // slider
@@ -44,15 +44,15 @@ let swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+        delay: 2500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
