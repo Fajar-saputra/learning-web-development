70fa84408ca8b72ccc8bb3c0939ea8c354e07244
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-01-08 22:12:57 +0700

    feat(footer): modify footer and add search button and overlay

--- projek/website-ecommerce-6/js/script.js
+++ projek/website-ecommerce-6/js/script.js
@@ -56,3 +56,15 @@ let swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+const searchButton = document.querySelector(".t-search"),
+    tClose = document.querySelector(".search-close"),
+    showClass = document.querySelector(".site");
+
+searchButton.addEventListener("click", function () {
+    showClass.classList.toggle('showsearch');
+});
+
+tClose.addEventListener("click", function () {
+    showClass.classList.remove("showsearch");
+});
