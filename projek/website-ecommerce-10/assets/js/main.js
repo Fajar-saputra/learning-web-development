405d3a9c907e014288a3421a60210452e6092963
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-02-10 21:24:00 +0700

    feat(projek/webstie-ecommerce-10): add section swiper-slide and scroll header

--- projek/website-ecommerce-10/assets/js/main.js
+++ projek/website-ecommerce-10/assets/js/main.js
@@ -1,52 +1,52 @@
 /*=============== SHOW MENU ===============*/
 
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW CART ===============*/
-
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
 
-
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW LOGIN ===============*/
-
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
 
-
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== HOME SWIPER ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
+/*=============== SHOW SCROLL UP ===============*/
+function scrollheader() {
+    const header = document.querySelector("#header");
+    // whin the scroll is greater than 58 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+}
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
+window.addEventListener("scroll", scrollheader);
 /*=============== LIGHT BOX ===============*/
 
-
 /*=============== QUESTIONS ACCORDION ===============*/
-
 
 /*=============== STYLE SWITCHER ===============*/
