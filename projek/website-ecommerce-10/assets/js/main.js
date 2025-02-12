b6956a71c3ecbca2ac624281efe147baa44e07e6
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-02-12 07:28:09 +0700

    feat(projek/webstie-ecommerce-10): add function for show user login,  show cart and scroll up button

--- projek/website-ecommerce-10/assets/js/main.js
+++ projek/website-ecommerce-10/assets/js/main.js
@@ -7,21 +7,42 @@
 /* Validate if constant exists */
 
 /*=============== SHOW CART ===============*/
+const cart = document.querySelector("#cart"),
+    cartShop = document.querySelector("#cart-shop"),
+    cartClose = document.querySelector("#cart-close");
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
-
+if (cartShop) {
+    cartShop.addEventListener("click", () => {
+        cart.classList.add("show-cart");
+    });
+}
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if (cartClose) {
+    cartClose.addEventListener("click", () => {
+        cart.classList.remove("show-cart");
+    });
+}
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.querySelector("#login"),
+    loginButton = document.querySelector("#login-button"),
+    loginClose = document.querySelector("#login-close");
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
-
+if (loginButton) {
+    loginButton.addEventListener("click", () => {
+        login.classList.add("show-login");
+    });
+}
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
-
+if (loginClose) {
+    loginClose.addEventListener("click", () => {
+        login.classList.remove("show-login");
+    });
+}
 /*=============== HOME SWIPER ===============*/
 var homeSwiper = new Swiper(".home-swiper", {
     spaceBetween: 30,
@@ -49,10 +70,16 @@ var newSwiper = new Swiper(".new-swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.querySelector("#scroll-up");
+    // when teh scroll is higher tan 350 viewport height, add the swhow scroll class to a tag with the scerool top class
+    if (this.scrollY >= 300) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+}
 
+window.addEventListener("scroll", scrollUp);
 /*=============== LIGHT BOX ===============*/
 
 /*=============== QUESTIONS ACCORDION ===============*/
 
 /*=============== STYLE SWITCHER ===============*/
-    
