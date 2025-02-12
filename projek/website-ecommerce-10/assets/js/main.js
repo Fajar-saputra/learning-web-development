9ca4405adadbe97686c57f9228ad2ccb75065a7e
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-02-12 22:06:57 +0700

    feat(projek/webstie-ecommerce-10): responsive css index.html

--- projek/website-ecommerce-10/assets/js/main.js
+++ projek/website-ecommerce-10/assets/js/main.js
@@ -1,9 +1,23 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.querySelector("#nav-menu"),
+    navToggle = document.querySelector("#nav-toggle"),
+    navClose = document.querySelector("#nav-close");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+        console.log("tess");
+    });
+}
 
 /*===== MENU HIDDEN =====*/
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
+
 /* Validate if constant exists */
 
 /*=============== SHOW CART ===============*/
