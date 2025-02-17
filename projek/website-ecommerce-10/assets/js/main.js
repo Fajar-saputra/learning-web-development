61b06d8800997e8963173cf0a88103cec7b9b48c
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-02-17 20:35:14 +0700

    feat(projek/website-ecommerce-10): add function for lightbox

--- projek/website-ecommerce-10/assets/js/main.js
+++ projek/website-ecommerce-10/assets/js/main.js
@@ -98,25 +98,64 @@ const productItems = document.querySelectorAll(".product__img"),
     lightBox = document.querySelector(".lightbox"),
     lightBoxImg = lightBox.querySelector(".lightbox__img"),
     lightBoxClose = lightBox.querySelector(".lightbox__close"),
-    lightBoxCounter = lightBox.querySelector(".lightbox__counter");
+    lightBoxCounter = lightBox.querySelector(".caption__counter");
 
 let itemIndex = 0;
 
-for (let i = 0; i < totalProductItems; i++){
-    productItems[i].addEventListener('click', () => {
+for (let i = 0; i < totalProductItems; i++) {
+    productItems[i].addEventListener("click", () => {
         itemIndex = i;
         changeItem();
-    })
+        toggleLightbox();
+    });
+}
+
+function nextItem() {
+    if (itemIndex == totalProductItems - 1) {
+        itemIndex = 0;
+    } else {
+        itemIndex++;
+    }
+
+    console.log(itemIndex);
+}
+
+function toggleLightbox() {
+    lightBox.classList.toggle("open");
 }
 
 function changeItem() {
-    imgSrc = productItems[itemIndex].querySelector('.portfolio__img img').getAttribute("src");
+    imgSrc = productItems[itemIndex].querySelector(".product__img img").getAttribute("src");
 
-    console.log(imgSrc)
+    lightBoxImg.src = imgSrc;
+    lightBoxCounter.innerHTML = itemIndex + " of " + totalProductItems;
+    console.log(imgSrc);
 }
-imgSrc = productItems[itemIndex].querySelector('.new__img').getAttribute("src");
 
-console.log(imgSrc)
+function nextItem() {
+    if (itemIndex == totalProductItems - 1) {
+        itemIndex = 0;
+    } else {
+        itemIndex++;
+    }
+    changeItem();
+}
+
+function prevItem() {
+    if (itemIndex == 0) {
+        itemIndex = totalProductItems - 1;
+    } else {
+        itemIndex--;
+    }
+    changeItem();
+}
+
+// close lightbox
+lightBox.addEventListener("click", () => {
+    if (event.target === lightBoxClose || event.target == lightBox) {
+        toggleLightbox();
+    }
+});
 
 /*=============== QUESTIONS ACCORDION ===============*/
 const accordionItem = document.querySelectorAll(".questions__item");
